7f64e5ce21b5e43cceb3fa2072243009e4e685ff
지수 박 <alyssapark96@gmail.com>
2021-01-01 11:47:41 +0100

    learn usage of props and state

--- src/App.js
+++ src/App.js
@@ -6,12 +6,27 @@ import Info from './components/Info';
 import { Component } from 'react';
 
 class App extends Component {
+  //state값을 초기화 시키는 과정 : constructor, 즉 생성자를 만드는 과정
+  //어떤 component가 실행될 때, contructor()함수가 가장 먼저 실행되면서 초기화를 담당한다.
+  constructor(props){
+    super(props);
+    //state값을 초기화 시키기 위한 코드
+    this.state={
+      subject: { title: 'Vegan for Everyone' },
+      contents: [
+        { id:1, title:'음식점', desc:'맛있는 비건 음식점을 찾아서'},
+        { id:2, title:'식료품점', desc:'품질 좋은 비건 식료품을 찾아서'},
+        { id:3, title:'그외 제품들',desc:'그 외 비건 제품들을 찾아서'},
+        { id:4, title:'로그인',desc:'그 외 비건 제품들을 찾아서'},
+      ]
+    }
+  }
+
   render(){
   return (
     <div className="App">
-      <Subject title="Vegan Trip"></Subject>
-      <Subject title="Vegan Place"></Subject>
-      <TOC></TOC>
+      <Subject title={this.state.subject.title}></Subject>
+      <TOC data={this.state.contents}></TOC>
       <Content></Content>
       <Info></Info>
     </div>
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,12 +2,17 @@ import { Component } from 'react';
 
 class TOC extends Component{
     render(){
+      var lists=[];
+      var data=this.props.data;
+      var i=0;
+      while(i<data.length){
+        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+        i=i+1;
+      }
       return(
         <nav>
           <ul>
-            <li><a href="Login">Login</a></li>
-            <li><a href="Playground">Playground</a></li>
-            <li><a href="Eat">Eat</a></li>
+            {lists}
           </ul>
         </nav>
       );
