2c2418cffd09c9654ab1853a7d1fc51797e11942
지수 박 <alyssapark96@gmail.com>
2021-01-03 22:35:52 +0100

    change state value of parent component using event

--- src/App.js
+++ src/App.js
@@ -12,14 +12,15 @@ class App extends Component {
     super(props);
     //state값을 초기화 시키기 위한 코드
     this.state={
-      mode:'welcome',
+      mode:'read',
+      selected_content_id:2,
       subject: { title: 'Vegan for Everyone', sub:"Good Vegan Places, not only for vegan but also for everyone" },
       welcome:{title: 'Welcome', desc:'Hello React'},
       contents: [
         { id:1, title:'음식점', desc:'맛있는 비건 음식점을 찾아서'},
         { id:2, title:'식료품점', desc:'품질 좋은 비건 식료품을 찾아서'},
         { id:3, title:'그외 제품들',desc:'그 외 비건 제품들을 찾아서'},
-        { id:4, title:'로그인',desc:'그 외 비건 제품들을 찾아서'},
+        { id:4, title:'로그인',desc:'로그인 정보'},
       ]
     }
   }
@@ -31,27 +32,33 @@ class App extends Component {
       _title=this.state.welcome.title;
       _desc=this.state.welcome.desc;
     }else if(this.state.mode === "read"){
-      _title=this.state.contents[0].title;
-      _desc=this.state.contents[0].desc;
+      var i=0;
+      while(i<this.state.contents.length){
+        var data=this.state.contents[i];
+        if(data.id===this.state.selected_content_id){
+          _title=data.title;
+          _desc=data.desc;
+          break;
+        }
+        i=i+1;
+      }
     }
   return (
     <div className="App">
-      {/* <Subject title={this.state.subject.title}></Subject> */}
-      <header>
-          <h1><a href="/" onClick={function(e){
-            console.log(e);
-            e.preventDefault(); //여기서 <a>태그의 기본적인 동작방법을 막기 위한 코드. 'e'는 event를 의미
-            //이 코드로는 state의 값이 변하지 않음!
-            this.state.mode='welcome'; // 에러가 나는 이유는 여기서 this가 가르키는 것이 없기 때문. --> bind(this) 사용하여 해결
-            this.setState({
-              mode:'welcome'
-            })
-          }.bind(this)}> {/* bind(this)를 함으로써 해당 함수 내에서 사용하는 this는 해당 component를 가르키게 됨*/}
-            {this.state.subject.title}
-            </a></h1>
-          {this.state.subject.sub}
-      </header>
-      <TOC data={this.state.contents}></TOC>
+      <Subject title={this.state.subject.title}
+      sub={this.state.subject.sub}
+      onChangePage={function(){
+        this.setState({
+          mode:'welcome'
+        })
+      }.bind(this)}></Subject>
+      <TOC data={this.state.contents}
+      onChangePage={function(id){
+        this.setState({
+          mode:'read',
+          selected_content_id:Number(id)
+        })
+      }.bind(this)}></TOC>
       <Content title={_title} desc={_desc}></Content>
       <Info></Info>
     </div>
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -4,8 +4,11 @@ class Subject extends Component{
     render(){
       return(
         <header>
-          <h1><a href="/">{this.props.title}</a></h1>
-          {this.state.subject.sub}
+          <h1><a href="/" onClick={function(e){
+            e.preventDefault(); //여기서 <a>태그의 기본적인 동작방법을 막기 위한 코드. 'e'는 event를 의미
+            this.props.onChangePage();
+          }.bind(this)}>{this.props.title}</a></h1>
+          {this.props.sub}
         </header>
       );
     }
