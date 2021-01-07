a920f03b250a44787b1076bab7fc321c9d571fa9
지수 박 <alyssapark96@gmail.com>
2021-01-07 22:35:38 +0100

    put banner section

--- src/App.js
+++ src/App.js
@@ -75,7 +75,12 @@ class App extends Component {
   return (
     <div className="App">
       <Menu></Menu>
-
+      <div style={{blockSize:128, padding:64, backgroundColor:'tomato'}}>
+        <p style={{textAlign:'center', fontWeight:'bold'}}>VECO = Vegan + Eco</p>
+        <p style={{textAlign:'center'}}>베코는 'Vegan for Everyone'이라는 생각에서 출발하였습니다.<br></br>
+        비건이라서 찾는 것이 아니라, 음식이 맛있어서, 제품이 좋아서 찾았으면 좋겠다는 바람으로 시작했습니다.
+       </p>
+      </div>
       <Subject title={this.state.subject.title}
       sub={this.state.subject.sub}
       onChangePage={function(){
--- src/components/Info.js
+++ src/components/Info.js
@@ -3,7 +3,7 @@ import { Component } from 'react';
 class Info extends Component{
     render(){
       return(
-        <footer>
+        <footer style={{backgroundColor:'tomato'}}>
           <p>Developer: Jisu Park</p>
           <p><a href="mailto:alyssapark96@gmail.com">Email: alyssapark96@gmail.com</a></p>
           <p>Phone Number: +82 010-1234-5678</p>
